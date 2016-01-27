84de9d1d7ac8f923ec4e26611693c48ef6b0d87b
reige012 <reige012@gmail.com>
2016-01-27 09:20:55 -0600

    Submitting Assignment 4

---
+++ Task 3/reigeltask3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# #encoding: utf-8
+
+"""
+This program uses a main function to make the range thing happen again.
+
+Created by Alicia Reigel. 26 January 2016.
+No copyright...anyone can use this.
+
+"""
+def alicia2():
+    print (*range(50,-1,-5), sep=',')
+
+def main():
+    alicia2()
+
+if __name__ == '__main__':
+    main()
---
+++ Task 4/reigeltask4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# #encoding: utf-8
+
+"""
+This program uses a main function print my favorite five words
+
+Created by Alicia Reigel. 26 January 2016.
+No copyright...anyone can use this.
+
+"""
+def word1():
+    print ("fork")
+def word2():
+    print ("happy")
+def word3():
+    print ("kitten")
+def word4():
+    print ("ocean")
+def word5():
+    print ("atmosphere")
+
+def main():
+    word1()
+    word2()
+    word3()
+    word4()
+    word5()
+
+if __name__ == '__main__':
+    main()
