b75b705069ab0de16615279d9b0a1d53f61e5dd0
Grace Cagle <gcagle1@lsu.edu>
2016-01-28 09:10:17 -0600

    assignment 4

---
+++ gcagle1/Task2/printing_is_fun.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+
+'''
+Printing is Fun by Grace Cagle
+
+Printing numbers 0-50 by 5
+
+Function name is pnum50
+
+'''
+
+
+def pnum50():
+        print(list(range(0, 55, 5)))
+
+if __name__ == '__main__':
+    pnum50()
