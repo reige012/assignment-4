1a7dab704564f9c075d30e77e919013de3b4cd48
pramodpantha <ppanth1@lsu.edu>
2016-01-28 09:43:56 -0600

    assignment_4

---
+++ answers/pramodpantha/task4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# utf-8
+
+"""
+list of five favourite words
+print the five favourite words seperated by newlines.
+Created by Pramod Pantha on 27 Jan, 2016.
+Copyright 2016 Pramod Pantha. All right reserved.
+
+"""
+
+
+def main():
+    print("life", "is", "pretty", "much", "beautiful", sep="\n")
+
+
+if __name__ == '__main__':
+    main()
