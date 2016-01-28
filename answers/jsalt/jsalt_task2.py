c8e2fbf4a67cedefa35b379b375a3d1df6cb0b72
Jessie Salter <jsalte5@lsu.edu>
2016-01-27 20:54:33 -0600

    Submitting assignment 4

---
+++ answers/jsalt/jsalt_task2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# utf-8
+
+"""
+Task 2 Program
+Jessie Salter
+26 January 2016
+"""
+
+# jsalt1() will print the numbers 0 to 50 by 5
+
+
+def jsalt1():
+    print(list(range(0, 55, 5)))
+
+
+# The main function calls the jsalt1() function
+
+
+def main():
+    jsalt1()
+
+
+if __name__ == '__main__':
+    main()
