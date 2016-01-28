b591832c9337ba8066bddce64d5031857f96fc5e
jonnations <jonnations@gmail.com>
2016-01-27 19:16:52 -0600

    tons answers

---
+++ answers/jonnations/task2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+program task 2. Assignment 4
+
+Jon Nations. 27 January 2016
+
+"""
+
+
+def main():
+
+    print(list(range(0, 51, 5)))
+
+if __name__ == '__main__':
+    main()
---
+++ answers/jonnations/task5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+program task 5. Assignment 4
+
+Jon Nations. 27 January 2016
+
+"""
+
+
+def main():
+
+    print('Myodes \nPithecheir \nDicrostonyx \nCarpomys \nPizza')
+
+
+if __name__ == '__main__':
+    main()
